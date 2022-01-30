90ad0a3e2b72daf26477030083c8f1d2cc2b3647
ba144220 <hs41511@gmail.com>
2022-01-30 20:52:00 +0800

    mongodb setup

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 // imports
 const express = require("express");
-
+const dotenv = require("dotenv");
+dotenv.config();
 const PORT = 5000;
 const app = express();
 
 console.log(__dirname);
 app.get("/", (req, res) => {
     //res.send("Hello to ntuchorus");
-    console.log("connect");
+    console.warn("connect");
     res.sendFile(__dirname + "/test/index.html");
 });
 app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`));
